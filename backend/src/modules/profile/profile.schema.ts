40d4fb77f3df640f1c156d67d05f1db5cfc4c890
FajilChauhan <chauhanfajil07@gmail.com>
2026-06-27 15:47:05 +0530

    fix image issue

--- backend/src/modules/profile/profile.schema.ts
+++ backend/src/modules/profile/profile.schema.ts
@@ -2,5 +2,5 @@ import { z } from "zod";
 
 export const updateProfileSchema = z.object({
   username: z.string().min(3, "Username must be at least 3 characters").optional(),
-  profileImage: z.string().url("Invalid image URL").optional(),
+  profileImage: z.string().optional(),
 });
